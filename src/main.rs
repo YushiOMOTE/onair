c62204ff143c6a3a3c01ced8392e5704aa264f32
YushiOMOTE <yushiomote@gmail.com>
2022-02-11 10:44:37 +0000

    Listen all

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ async fn main() -> std::io::Result<()> {
             .route("/onair", web::get().to(show))
             .route("/onair", web::post().to(update))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
