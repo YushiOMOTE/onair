75f70faaebe7f0fb28c54d6126cba510b2096d09
YushiOMOTE <yushiomote@gmail.com>
2022-02-12 07:37:22 +0900

    Support option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_files::NamedFile;
 use actix_web::{middleware::Logger, web, App, HttpServer, Result};
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
@@ -35,9 +36,20 @@ async fn show(state: web::Data<Mutex<State>>) -> Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short = 'p', long = "port")]
+    port: u16,
+    #[clap(short = 'd', long = "debug")]
+    debug: bool,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
+    let args = Args::parse();
+    let level = if args.debug { "debug" } else { "info" };
+    std::env::set_var("RUST_LOG", format!("actix_web={}", level));
+
     env_logger::init();
 
     let data = web::Data::new(Mutex::new(State::new()));
@@ -49,7 +61,7 @@ async fn main() -> std::io::Result<()> {
             .route("/onair", web::get().to(show))
             .route("/onair", web::post().to(update))
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", args.port))?
     .run()
     .await
 }
